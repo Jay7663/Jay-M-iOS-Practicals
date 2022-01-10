9f91f6e2f8891a4962a7f40211e1c82ff9d5f533
Jay Modhawadiya <jay.modhawadiya@gmail.com>
2022-01-10 19:07:37 +0530

    Initial Commit (Empty project with Button)

---
+++ iOSPracticals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSPracticals
+//
+//  Created by Modhawadiya Jay on 10/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
